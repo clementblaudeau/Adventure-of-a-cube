8851194bc3ac9921b4c3d33df597e2f059032653
Clément <thefractaler@gmail.com>
2012-02-25 11:05:17 +0100

    Ajout de fichiers utiles au projet git.

---
+++ credits.py
@@ -0,0 +1,29 @@
+
+# -*- coding: utf-8 -*-
+
+#------------------------------#
+# 		Menu Principal.py	   #
+#		Clement Blaudeau	   #
+#			******			   #
+#------------------------------#
+
+
+import pygame
+from pygame.locals import *
+import general
+
+class Credits:
+	def __init__(self):
+		self.e = 2
+		
+	def Affichage(self, fenetre):
+		image = pygame.image.load("images/credits.png").convert_alpha()
+		continuer = 1
+		while continuer:
+			pygame.display.flip()
+			fenetre.blit(image, (0,0))
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					continuer = 0
+				if event.type == KEYDOWN:
+					continuer = 0
---
+++ general.py
@@ -0,0 +1,20 @@
+
+
+h = 480
+w = 640
+ennemis = 0
+niv = 0
+tirs = 0
+avancement = 0
+scrool = -544
+
+k_left = 276
+k_right = 275
+k_up = 273
+k_down = 274
+k_shot = 97
+k_slow = 122
+
+
+
+
