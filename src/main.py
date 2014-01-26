34de10f9f1d759bd00847b2726dc9dcf7e376746
Clément Blaudeau <thefractaler@gmail.com>
2014-01-26 21:52:06 +0100

    Reprise du Projet.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-Copyright (c) 2012 Clément Blaudeau
+Copyright (c) 2014 Clément Blaudeau
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
